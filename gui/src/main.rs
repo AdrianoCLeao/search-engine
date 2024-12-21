3d308f3bffc79e987c057e524edb27dc0515ba6a
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-21 16:48:04 -0300

    feat: using eframe to generate a GUI

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -5,12 +5,73 @@ use bindings::*;
 
 use std::ffi::CString;
 
-fn main() {
-    unsafe {
-        let mut engine: TFIDFEngine = std::mem::zeroed();
+use eframe::egui;
 
-        let query = CString::new("Alice's").unwrap();
+fn main() -> eframe::Result {
+    env_logger::init();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Search Engine",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
 
-        tfidf_search(&mut engine, query.as_ptr());
+struct MyApp {
+    input_text: String,
+    result_text: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            input_text: String::new(),
+            result_text: String::from("Enter a query and click search."),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Search bar implemented based on TF-IDF algorithm");
+            ui.horizontal(|ui| {
+                let input_label = ui.label("Your search: ");
+                ui.text_edit_singleline(&mut self.input_text)
+                    .labelled_by(input_label.id);
+            });
+
+            if ui.button("Search").clicked() {
+                let result = self.perform_search(&self.input_text);
+                self.result_text = result;
+            }
+
+            ui.label("Results:");
+            ui.label(&self.result_text);
+        });
+    }
+}
+
+impl MyApp {
+    fn perform_search(&self, query: &str) -> String {
+        if query.is_empty() {
+            return "Query cannot be empty.".to_string();
+        }
+
+        let result = unsafe {
+            let mut engine: TFIDFEngine = std::mem::zeroed();
+            let query_cstring = CString::new(query).expect("Failed to convert query to CString");
+
+            tfidf_search(&mut engine, query_cstring.as_ptr());
+            format!(
+                "Search completed for '{}'.",
+                query
+            )
+        };
+
+        result
     }
 }
