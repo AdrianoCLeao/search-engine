f0bc19b075642594d2537ec79e06f1f356060df2
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-21 23:24:34 -0300

    feat: last methods to handle json

--- engine/src/json/json.c
+++ engine/src/json/json.c
@@ -1,4 +1,4 @@
-#include "json.h"
+#include "../include/json/json.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,5 +38,68 @@ char* json_objClose(char* dest, size_t* remLen) {
         (*remLen)++;
     }
     dest = atoa(dest, "},", remLen);
+    return dest;
+}
+
+char* json_arrOpen(char* dest, const char* name, size_t* remLen) {
+    if (name) {
+        dest = chtoa(dest, '\"', remLen);
+        dest = atoa(dest, name, remLen);
+        dest = atoa(dest, "\":[", remLen);
+    } else {
+        dest = chtoa(dest, '[', remLen);
+    }
+    return dest;
+}
+
+char* json_arrClose(char* dest, size_t* remLen) {
+    if (dest[-1] == ',') {
+        dest[-1] = '\0';
+        (*remLen)++;
+    }
+    dest = atoa(dest, "],", remLen);
+    return dest;
+}
+
+char* json_nstr(char* dest, const char* name, const char* value, size_t* remLen) {
+    if (name) {
+        dest = chtoa(dest, '\"', remLen);
+        dest = atoa(dest, name, remLen);
+        dest = atoa(dest, "\":\"", remLen);
+    }
+    dest = atoa(dest, value, remLen);
+    dest = atoa(dest, "\",", remLen);
+    return dest;
+}
+
+char* json_num(char* dest, const char* name, double value, size_t* remLen) {
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "%g", value);
+    if (name) {
+        dest = chtoa(dest, '\"', remLen);
+        dest = atoa(dest, name, remLen);
+        dest = atoa(dest, "\":", remLen);
+    }
+    dest = atoa(dest, buffer, remLen);
+    dest = chtoa(dest, ',', remLen);
+    return dest;
+}
+
+char* json_bool(char* dest, const char* name, int value, size_t* remLen) {
+    if (name) {
+        dest = chtoa(dest, '\"', remLen);
+        dest = atoa(dest, name, remLen);
+        dest = atoa(dest, "\":", remLen);
+    }
+    dest = atoa(dest, value ? "true" : "false", remLen);
+    dest = chtoa(dest, ',', remLen);
+    return dest;
+}
+
+char* json_finalize(char* dest, size_t* remLen) {
+    if (dest[-1] == ',') {
+        dest[-1] = '\0';
+        (*remLen)++;
+    }
     return dest;
 }
