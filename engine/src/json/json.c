4981e9973e258dfd8c3cb135d5abfd3ab47affb5
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-21 23:21:23 -0300

    feat: methods to open and close jsons

--- engine/src/json/json.c
+++ engine/src/json/json.c
@@ -20,3 +20,23 @@ static char* atoa(char* dest, const char* src, size_t* remLen) {
     *dest = '\0';
     return dest;
 }
+
+char* json_objOpen(char* dest, const char* name, size_t* remLen) {
+    if (name) {
+        dest = chtoa(dest, '\"', remLen);
+        dest = atoa(dest, name, remLen);
+        dest = atoa(dest, "\":{", remLen);
+    } else {
+        dest = chtoa(dest, '{', remLen);
+    }
+    return dest;
+}
+
+char* json_objClose(char* dest, size_t* remLen) {
+    if (dest[-1] == ',') {
+        dest[-1] = '\0';
+        (*remLen)++;
+    }
+    dest = atoa(dest, "},", remLen);
+    return dest;
+}
