b31f07ce3b74a2e8dbad5be18b4c0edca7d09ebf
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-21 23:20:52 -0300

    feat: new functions to handle strings because C

---
+++ engine/src/json/json.c
@@ -0,0 +1,22 @@
+#include "json.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static char* chtoa(char* dest, char ch, size_t* remLen) {
+    if (*remLen > 0) {
+        *dest++ = ch;
+        *dest = '\0';
+        (*remLen)--;
+    }
+    return dest;
+}
+
+static char* atoa(char* dest, const char* src, size_t* remLen) {
+    while (*src && *remLen > 0) {
+        *dest++ = *src++;
+        (*remLen)--;
+    }
+    *dest = '\0';
+    return dest;
+}
