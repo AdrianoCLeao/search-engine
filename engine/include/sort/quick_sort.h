e19352d5893e887e6ae6a0d38dc6aa06f9079ab2
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-23 22:05:20 -0300

    chore: updating code files with the GNU GLPv3 license

--- engine/include/sort/quick_sort.h
+++ engine/include/sort/quick_sort.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of TFIDF Search Engine.
+ *
+ * Copyright (C) 2024 Adriano Leão
+ *
+ * TFIDF Search Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TFIDF Search Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TFIDF Search Engine. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
