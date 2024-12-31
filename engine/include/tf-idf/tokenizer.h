267e2770eeb843ec7d17b19de325fecb41b59bbf
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-30 23:45:48 -0300

    refactor: tokenizing logic separated onto tokenizer.c and tokenizer.h

---
+++ engine/include/tf-idf/tokenizer.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of TFIDF Search Engine.
+ *
+ * Copyright (C) 2024 Adriano Leão
+ *
+ * TFIDF Search Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TFIDF Search Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TFIDF Search Engine. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include "../tf-idf/tfidf_engine.h"
+
+void tokenize_to_file(const char *content, const char *output_file);
+int term_exists(const char *term, TermData terms[], int term_count);
+
+#endif
